9b37b24a2f4e84657cbcec2024b7dbccc96f229f
Rishab Kumar <rishabkumar7@gmail.com>
2024-08-13 15:32:12 -0400

    updated main.py

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_community.llms import Ollama
+from langchain.prompts import PromptTemplate
 
-llm = Ollama(
-    model="gemma"
-)
+from dotenv import load_dotenv
+load_dotenv()
 
-response = llm.invoke("Tell me a joke")
-print(response)
+def ask_stoic(query):
+    llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
+
+#   llm = Ollama(model="gemma")
+
+    prompt = PromptTemplate(
+        input_variables=["query"],
+        template = """
+        You are a stoic assistant that answers and helps people with dilemma and concerns in life with Stoicism. Here is the user query: {query}
+        """
+    )
+
+    chain = prompt | llm
+
+    response = chain.invoke({"query":query})
+    return response.content
+
+print(ask_stoic("How to stay calm in difficult situations?"))
